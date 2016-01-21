f1cd44492e50664b391fb23e5e293435235e3818
jwhitwo <jnwhitwo@uncg.edu>
2016-01-21 13:49:08 -0500

    Initial creation of starter program.

---
+++ MyString.java
@@ -0,0 +1,11 @@
+/*
+ * Author: 
+ * Program 1 - MyString
+ * CSC230-02 Spring 2016
+ */
+package program1;
+
+public class MyString {
+    public MyString()
+    {}
+}
---
+++ Program1.java
@@ -0,0 +1,17 @@
+/*
+ * Author: 
+ * Program 1 - MyString
+ * CSC230-02 Spring 2016
+ */
+package program1;
+
+public class Program1 {
+
+    public static void main(String[] args) {
+        //replace <name> with your full name
+        System.out.println("MyString implementation by <name>.");
+        
+        //anything below here will be erased
+    }
+    
+}
